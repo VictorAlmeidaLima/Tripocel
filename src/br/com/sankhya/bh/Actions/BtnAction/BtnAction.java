26538d6408c146cd74a1c944ff39b2d0c5e963c0
VICTOR ALCIDES <victor.alcides@sankhya.com.br>
2019-07-05 09:56:13 -0300

    Commit Inicial

---
+++ src/br/com/sankhya/bh/Actions/BtnAction/BtnAction.java
@@ -0,0 +1,12 @@
+package br.com.sankhya.bh.Actions.BtnAction;
+
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+
+public class BtnAction implements AcaoRotinaJava {
+
+    @Override
+    public void doAction(ContextoAcao contextoAcao) throws Exception {
+
+    }
+}
